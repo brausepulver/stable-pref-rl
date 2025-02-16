b94fc1a48b77d4250c001631ac1929cdaff373ef
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-02-16 13:02:29 +0100

    feat: allow reward model training on mps/cuda

--- src/pref_rl/callbacks/pref_ppo.py
+++ src/pref_rl/callbacks/pref_ppo.py
@@ -23,6 +23,7 @@ class RewardModel(nn.Module):
 
 class PrefPPOCallback(BasePrefCallback):
     def __init__(self,
+        device: str = 'cpu',
         n_epochs_reward: int = 100,
         train_acc_threshold_reward: float = 0.97,
         learning_rate_reward: float = 3e-4,
@@ -37,6 +38,13 @@ class PrefPPOCallback(BasePrefCallback):
         self.batch_size_reward = batch_size_reward
         self.reward_model_kwargs = reward_model_kwargs
         self.lr_reward = learning_rate_reward
+
+        self.device = torch.device(
+            device or
+            'mps' if torch.backends.mps.is_available() else
+            'cuda' if torch.cuda.is_available() else
+            'cpu'
+        )
 
 
     def _init_callback(self):
@@ -59,6 +67,8 @@ class PrefPPOCallback(BasePrefCallback):
 
 
     def _compute_loss_for_member(self, member: nn.Module, segments: torch.Tensor, preferences: torch.Tensor):
+        segments = segments.to(self.device)
+        preferences = preferences.to(self.device)
         pred_rewards = member(segments)
         pred_returns = einops.reduce(pred_rewards, 'b n s 1 -> b n', 'sum')
         loss = self.rew_loss(pred_returns, preferences)
@@ -68,7 +78,7 @@ class PrefPPOCallback(BasePrefCallback):
 
     def _train_member_epoch(self, member: nn.Module):
         dataset = TensorDataset(self.segment_buffer, self.preference_buffer)
-        dataloader = DataLoader(dataset, batch_size=self.batch_size_reward, shuffle=True)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size_reward, shuffle=True, pin_memory=self.device.type == 'cuda')
         losses = []
         accuracies = []
 
@@ -91,6 +101,8 @@ class PrefPPOCallback(BasePrefCallback):
 
     def _train_predictor(self):
         self.reward_model.train()
+        self.reward_model.to(self.device)
+
         losses = []
         accuracies = []
 
@@ -109,6 +121,8 @@ class PrefPPOCallback(BasePrefCallback):
         with torch.no_grad():
             self._validate_reward_model()
 
+        self.reward_model.to(self.model.device)
+
 
     def _validate_reward_model(self):
         self.reward_model.eval()
@@ -117,13 +131,13 @@ class PrefPPOCallback(BasePrefCallback):
         preferences, keep_indices = self.teacher.query_segments(rewards)
 
         dataset = TensorDataset(segments[keep_indices], preferences)
-        dataloader = DataLoader(dataset, batch_size=self.batch_size_reward, shuffle=True)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size_reward, shuffle=True, pin_memory=self.device.type == 'cuda')
 
         batch_statistics = [self._compute_loss_for_member(member, *batch) for batch in dataloader for member in self.reward_model.members]
         batch_losses, batch_accuracies = zip(*batch_statistics)
 
-        self.logger.record('reward_model/eval/loss', np.mean(batch_losses))
-        self.logger.record('reward_model/eval/accuracy', np.mean(batch_accuracies))
+        self.logger.record('reward_model/eval/loss', torch.tensor(batch_losses).mean().item())
+        self.logger.record('reward_model/eval/accuracy', torch.tensor(batch_accuracies).mean().item())
 
 
     def _predict_rewards(self):
