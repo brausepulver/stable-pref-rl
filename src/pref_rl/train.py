6061b8ea09d76dcebd2962582c5f0506c79506b2
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-02-18 14:42:15 +0100

    feat: make runnable without wandb

--- src/pref_rl/train.py
+++ src/pref_rl/train.py
@@ -1,11 +1,11 @@
 import hydra
+from importlib.util import find_spec
 from omegaconf import DictConfig, OmegaConf
 import os
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.callbacks import EvalCallback, CallbackList
 from stable_baselines3.common.vec_env import VecNormalize
-import wandb
-from wandb.integration.sb3 import WandbCallback
+import uuid
 from .envs import make_vec_env as make_env
 
 
@@ -13,9 +13,15 @@ CONFIG_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../../configs")
 )
 
+has_wandb = find_spec("wandb") is not None
+_WANDB = None
+
 
 def setup_wandb(cfg: DictConfig):
-    run = wandb.init(
+    global _WANDB
+    import wandb
+    _WANDB = wandb
+    run = _WANDB.init(
         project=cfg.logging.project,
         group=cfg.logging.group,
         tags=cfg.logging.tags,
@@ -27,8 +33,10 @@ def setup_wandb(cfg: DictConfig):
 
 @hydra.main(config_path=CONFIG_DIR, config_name="config", version_base="1.3")
 def main(cfg: DictConfig) -> None:
-    # Setup WandB
-    run = setup_wandb(cfg)
+    global _WANDB
+
+    # Setup run
+    run_id = setup_wandb(cfg).id if has_wandb else uuid.uuid4()
 
     # Create environment
     if 'policy' in cfg.preset.env:
@@ -43,15 +51,20 @@ def main(cfg: DictConfig) -> None:
     eval_env = make_env(**cfg.preset.env)
 
     # Setup callbacks
-    eval_callback = EvalCallback(
-        eval_env,
-        best_model_save_path="./best_model",
-        log_path="./logs",
-        eval_freq=cfg.logging.eval_freq,
-        deterministic=True,
-        render=False
-    )
-    wandb_callback = WandbCallback()
+    callbacks = [
+        EvalCallback(
+            eval_env,
+            best_model_save_path="./best_model",
+            log_path="./logs",
+            eval_freq=cfg.logging.eval_freq,
+            deterministic=True,
+            render=False
+        )
+    ]
+    if has_wandb:
+        from wandb.integration.sb3 import WandbCallback
+        callback = WandbCallback()
+        callbacks.append(callback)
 
     # Create model
     model: BaseAlgorithm = hydra.utils.instantiate(
@@ -59,7 +72,7 @@ def main(cfg: DictConfig) -> None:
         policy,
         env,
         verbose=1,
-        tensorboard_log=f"runs/{run.id}",
+        tensorboard_log=f"runs/{run_id}",
         seed=cfg.training.seed,
         # Hydra arguments
         _convert_="all"
@@ -70,27 +83,28 @@ def main(cfg: DictConfig) -> None:
         log_freq_rollouts = cfg.preset.get('logging', {}).get('log_freq_rollouts')
         model.learn(
             total_timesteps=cfg.training.total_timesteps,
-            callback=CallbackList([eval_callback, wandb_callback]),
+            callback=CallbackList(callbacks),
             progress_bar=True,
             **({'log_interval': log_freq_rollouts} if log_freq_rollouts else {})
         )
 
         # Save final model
-        model.save(f"final_model_{run.id}")
+        model.save(f"final_model_{run_id}")
 
         # Save normalized env stats
         if isinstance(env, VecNormalize):
-            env.save(f"vec_normalize_{run.id}.pkl")
+            env.save(f"vec_normalize_{run_id}.pkl")
 
     except KeyboardInterrupt:
         print("Saving model...")
-        model.save(f"interrupted_model_{run.id}")
+        model.save(f"interrupted_model_{run_id}")
 
     finally:
         # Close environments
         env.close()
         eval_env.close()
-        wandb.finish()
+        if has_wandb:
+            _WANDB.finish()
 
 
 if __name__ == "__main__":
