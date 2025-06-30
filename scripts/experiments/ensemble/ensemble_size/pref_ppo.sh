d178b3655963bd524446e2d6091a02b7af1ff1e4
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-06-30 21:08:55 +0200

    feat: add ensemble_size and limited_eps experiments

---
+++ scripts/experiments/ensemble/ensemble_size/pref_ppo.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+BASE_PARAMS=(
+    "preset=pref_ppo/quadruped_walk"
+    "training.total_timesteps=2000000"
+    "preset.method.clip_range.end=0.2"
+    "training.num_envs=16"
+    "preset.env.limit_ep_steps=1000"
+    "preset.method.unsuper.n_steps_unsuper=32000"
+    "preset.method.pref.n_steps_reward=32000"
+    "preset.method.pref.teacher=oracle"
+    "preset.method.pref.device=cpu"
+    "preset.method.pref.sampler=entropy"
+)
+
+for ensemble_size in 3 5 7; do
+    echo "Running experiments with ensemble_size=${ensemble_size}"
+
+    for i in $(seq 1 8); do
+        seed=$((1000 * i))
+
+        train \
+            ${BASE_PARAMS[@]} \
+            training.seed=$seed \
+            "preset.method.pref.reward_model_kwargs.ensemble_size=${ensemble_size}" \
+            "logging.tags=[pref_ppo, experiment, entropy, ensemble_size]" \
+            "logging.group=pref_ppo/ensemble_size/entropy/${ensemble_size}" &
+    done
+    wait
+done
---
+++ scripts/experiments/limited_eps/pref_ppo.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+BASE_PARAMS=(
+    "preset=pref_ppo/quadruped_walk"
+    "training.total_timesteps=2000000"
+    "preset.method.clip_range.end=0.2"
+    "training.num_envs=16"
+    "preset.env.limit_ep_steps=1000"
+    "preset.method.unsuper.n_steps_unsuper=32000"
+    "preset.method.pref.n_steps_reward=32000"
+    "preset.method.pref.teacher=oracle"
+    "preset.method.pref.device=cpu"
+    "preset.method.pref.sampler=entropy"
+)
+
+for ann_buffer_size_eps in null 128 64 32 16; do
+    echo "Running experiments with ann_buffer_size_eps=${ann_buffer_size_eps}"
+
+    for i in $(seq 1 8); do
+        seed=$((1000 * i))
+
+        train \
+            ${BASE_PARAMS[@]} \
+            training.seed=$seed \
+            "preset.method.pref.ann_buffer_size_eps=${ann_buffer_size_eps}" \
+            "logging.tags=[pref_ppo, experiment, entropy, ann_buffer_size_eps]" \
+            "logging.group=pref_ppo/ann_buffer_size_eps/entropy/${ann_buffer_size_eps}" &
+    done
+    wait
+done
