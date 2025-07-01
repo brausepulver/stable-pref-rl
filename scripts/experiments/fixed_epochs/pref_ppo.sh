b28479843727e957d7d56f37bd5038bea66f1267
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-07-01 04:46:38 +0200

    feat: add acc_threshold and fixed_epochs experiments

---
+++ scripts/experiments/fixed_epochs/pref_ppo.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+BASE_PARAMS=(
+    "preset=pref_ppo/quadruped_walk"
+    "training.total_timesteps=2000000"
+    "preset.method.clip_range.end=0.2"
+    "training.num_envs=16"
+    "preset.env.limit_ep_steps=1000"
+    "preset.method.unsuper.n_steps_unsuper=32000"
+    "preset.method.pref.n_steps_reward=32000"
+    "preset.method.pref.teacher=oracle"
+    "preset.method.pref.device=cpu"
+    "preset.method.pref.sampler=disagreement"
+)
+
+for n_epochs_reward in 1 2 5 10; do
+    echo "Running experiments with n_epochs_reward=${n_epochs_reward}"
+
+    for i in $(seq 1 8); do
+        seed=$((1000 * i))
+
+        train \
+            ${BASE_PARAMS[@]} \
+            training.seed=$seed \
+            "preset.method.pref.train_acc_threshold_reward=0" \
+            "preset.method.pref.n_epochs_reward=${n_epochs_reward}" \
+            "logging.tags=[pref_ppo, experiment, disagreement, disjoint_data, n_epochs_reward]" \
+            "logging.group=pref_ppo/disjoint_data/disagreement/${n_epochs_reward}" &
+    done
+    wait
+done
