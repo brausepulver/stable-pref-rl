2dc3dc0b49bba05a3a27482c394048135c060b9c
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-07-12 00:26:01 +0200

    feat: log eval ensemble loss and accuracy

--- src/pref_rl/callbacks/pref_ppo.py
+++ src/pref_rl/callbacks/pref_ppo.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
+from typing import Callable
+
 from .pref import BasePrefCallback
 from ..utils.model import build_layered_module
 from ..utils.pref import NoValidEpisodesError, Teacher
@@ -31,14 +33,15 @@ class PrefPPOCallback(BasePrefCallback):
         learning_rate_reward: float = 3e-4,
         batch_size_reward: int = 128,
         reward_model_kwargs: dict = {},
-        n_steps_eval_current: int = None,
+        n_steps_eval_current: int | None = None,
         train_members_sequential: bool = True,  # For consistency with B-Pref
         validate_on_train: bool = False,
         validate_on_current: bool = True,
         validate_on_held_out: bool = True,
-        held_out_data_path: str = None,
+        held_out_data_path: str | None = None,
         log_sampler_metrics: bool = True,
         ensemble_disjoint_data: bool = False,
+        ensemble_agg_fn: Callable = lambda pred: pred.mean(dim=0),
         **kwargs
     ):
         super().__init__(log_sampler_metrics=log_sampler_metrics, **kwargs)
@@ -55,6 +58,7 @@ class PrefPPOCallback(BasePrefCallback):
         self.validate_on_held_out = validate_on_held_out
         self.log_sampler_metrics = log_sampler_metrics
         self.ensemble_disjoint_data = ensemble_disjoint_data
+        self.ensemble_agg_fn = ensemble_agg_fn
 
         self.held_out_data_path = (
             Path(to_absolute_path(held_out_data_path))
@@ -98,20 +102,21 @@ class PrefPPOCallback(BasePrefCallback):
         return correct.mean().item()
 
 
-    def _compute_loss_for_module(self, module: nn.Module, segments: torch.Tensor, preferences: torch.Tensor, module_is_ensemble: bool = False):
+    def _calculate_loss(self, pred_rewards: torch.Tensor, preferences: torch.Tensor):
+        pred_returns = einops.reduce(pred_rewards, '... batch pair segment 1 -> ... batch pair', 'sum')
+        inputs = pred_returns[..., 1] - pred_returns[..., 0]
+        loss = self.bce_loss(inputs, preferences)
+        accuracy = self._calculate_accuracy(pred_returns, preferences)
+        return loss, accuracy
+
+
+    def _predict_and_compute_loss(self, module: nn.Module, segments: torch.Tensor, preferences: torch.Tensor, module_is_ensemble: bool = False):
         segments = segments.to(self.device)
         preferences = preferences.to(self.device)
         pred_rewards = module(segments)
-
-        pred_returns = einops.reduce(pred_rewards, '... batch pair segment 1 -> ... batch pair', 'sum')
         if module_is_ensemble:
-            pred_returns = pred_returns.mean(dim=0)
-
-        inputs = pred_returns[..., 1] - pred_returns[..., 0]
-        loss = self.bce_loss(inputs, preferences)
-
-        accuracy = self._calculate_accuracy(pred_returns, preferences)
-        return loss, accuracy
+            pred_rewards = self.ensemble_agg_fn(pred_rewards)
+        return self._calculate_loss(pred_rewards, preferences)
 
 
     def _train_module_epoch(self, module: nn.Module, dataset: TensorDataset, module_is_ensemble: bool = False):
@@ -122,7 +127,7 @@ class PrefPPOCallback(BasePrefCallback):
         for segments, preferences in dataloader:
             self.rew_optimizer.zero_grad()
 
-            loss, accuracy = self._compute_loss_for_module(module, segments, preferences, module_is_ensemble)
+            loss, accuracy = self._predict_and_compute_loss(module, segments, preferences, module_is_ensemble)
             loss.backward()
             self.rew_optimizer.step()
 
@@ -203,8 +208,24 @@ class PrefPPOCallback(BasePrefCallback):
 
         dataset = TensorDataset(segments, preferences)
         dataloader = DataLoader(dataset, batch_size=self.batch_size_reward, pin_memory=self.device.type == 'cuda')
-        batch_statistics = [self._compute_loss_for_module(member, *batch) for batch in dataloader for member in self.reward_model.members]
-        batch_losses, batch_accuracies = zip(*batch_statistics)
+
+        batch_losses, batch_accuracies = [], []
+        member_losses, member_accuracies = [], []
+
+        for segments, preferences in dataloader:
+            segments = segments.to(self.device)
+            preferences = preferences.to(self.device)
+
+            ensemble_preds = self.reward_model(segments)
+            for member_preds in ensemble_preds:
+                loss, accuracy = self._calculate_loss(member_preds, preferences)
+                member_losses.append(loss.item())
+                member_accuracies.append(accuracy)
+
+            pred_rewards = self.ensemble_agg_fn(ensemble_preds)
+            loss, accuracy = self._calculate_loss(pred_rewards, preferences)
+            batch_losses.append(loss.item())
+            batch_accuracies.append(accuracy)
 
         batch_corrs = []
         if compute_correlation:
@@ -213,55 +234,63 @@ class PrefPPOCallback(BasePrefCallback):
             dataloader = DataLoader(dataset, batch_size=self.batch_size_reward, pin_memory=self.device.type == 'cuda')
 
             for segments, rewards in dataloader:
-                pred_rewards = self.reward_model(segments.to(self.device)).mean(dim=0).squeeze(-1)
-                corr_coef = self._calculate_corr_coef(pred_rewards, rewards.to(self.device))
+                member_pred_rew = self.reward_model(segments.to(self.device))
+                member_rewards = self.ensemble_agg_fn(member_pred_rew).squeeze(-1)
+                corr_coef = self._calculate_corr_coef(member_rewards, rewards.to(self.device))
                 batch_corrs.append(corr_coef)
 
-        return (
-            torch.tensor(batch_losses).mean().item(),
-            torch.tensor(batch_accuracies).mean().item(),
-            torch.tensor(batch_corrs).mean().item() if compute_correlation else None,
-        )
+        return {
+            'loss': np.mean(batch_losses),
+            'accuracy': np.mean(batch_accuracies),
+            'corr_coef': np.mean(batch_corrs) if batch_corrs else None,
+            'member_loss': np.mean(member_losses),
+            'member_accuracy': np.mean(member_accuracies)
+        }
 
 
     def _validate_on_episodes(self, episodes, size: int, compute_correlation: bool = False, compute_sampler_metrics: bool = True):
         segments, rewards, sampler_metrics = self.sampler.sample_segments(episodes, size, 'uniform', self.reward_model, compute_uniform_metrics=compute_sampler_metrics)
         preferences, keep_indices = self.eval_teacher.query_segments(rewards)
-        return (
-            *self._validate_on_segments(segments[keep_indices], rewards[:, keep_indices], preferences, compute_correlation),
-            sampler_metrics
-        )
+        return {
+            **self._validate_on_segments(segments[keep_indices], rewards[:, keep_indices], preferences, compute_correlation),
+            'sampler_metrics': sampler_metrics
+        }
 
 
-    def _log_validation_metrics(self, loss: float, accuracy: float, corr_coef: float | None, sampler_metrics: dict | None, prefix: str = ''):
-        self.logger.record(f"reward_model/eval/{prefix}loss", loss)
-        self.logger.record(f"reward_model/eval/{prefix}accuracy", accuracy)
-        if corr_coef:
-            self.logger.record(f"reward_model/eval/{prefix}corr_coef", corr_coef)
+    def _log_validation_metrics(self, metrics: dict, prefix: str = ''):
+        self.logger.record(f"reward_model/eval/{prefix}loss", metrics.get('loss'))
+        self.logger.record(f"reward_model/eval/{prefix}accuracy", metrics.get('accuracy'))
+        self.logger.record(f"reward_model/eval/{prefix}member_loss", metrics.get('member_loss'))
+        self.logger.record(f"reward_model/eval/{prefix}member_accuracy", metrics.get('member_accuracy'))
+
+        if metrics.get('corr_coef') is not None:
+            self.logger.record(f"reward_model/eval/{prefix}corr_coef", metrics.get('corr_coef'))
+
+        sampler_metrics = metrics.get('sampler_metrics')
         if self.log_sampler_metrics and sampler_metrics:
             self._log_sampler_metrics(sampler_metrics, prefix=prefix)
 
         if self.run:
             self.run.log({
-                f'reward_model/eval/{prefix}loss': loss,
-                f'reward_model/eval/{prefix}accuracy': accuracy,
-                **({ f'reward_model/eval/{prefix}corr_coef': corr_coef } if corr_coef is not None else {}),
+                f'reward_model/eval/{prefix}loss': metrics.get('loss'),
+                f'reward_model/eval/{prefix}accuracy': metrics.get('accuracy'),
+                **({ f'reward_model/eval/{prefix}corr_coef': metrics.get('corr_coef') } if metrics.get('corr_coef') is not None else {}),
                 'pref/num_feed': self.num_feed,
                 'pref/training_progress': self.training_progress,
             })
 
 
     def _validate_train(self):
-        loss, accuracy, corr_coef, sampler_metrics = self._validate_on_episodes(self.buffer.get_episodes(), len(self.segment_buffer))
-        self._log_validation_metrics(loss, accuracy, corr_coef, sampler_metrics)
+        metrics = self._validate_on_episodes(self.buffer.get_episodes(), len(self.segment_buffer))
+        self._log_validation_metrics(metrics)
 
 
     def _validate_current(self):
         episodes = self.buffer.get_episodes()[-self.done_eps_since_eval:]
         self.done_eps_since_eval = 0
         try:
-            loss, accuracy, corr_coef, sampler_metrics = self._validate_on_episodes(episodes, int(0.5 * sum(len(ep) for ep in episodes) / self.segment_size))
-            self._log_validation_metrics(loss, accuracy, corr_coef, sampler_metrics, prefix='current/')
+            metrics = self._validate_on_episodes(episodes, int(0.5 * sum(len(ep) for ep in episodes) / self.segment_size))
+            self._log_validation_metrics(metrics, prefix='current/')
         except NoValidEpisodesError:
             return
 
@@ -277,9 +306,8 @@ class PrefPPOCallback(BasePrefCallback):
         preferences, keep_indices = self.eval_teacher.query_segments(rewards)
         sampler_metrics = self.sampler.compute_metrics(segments, self.reward_model)
 
-        loss, accuracy, corr_coef = self._validate_on_segments(segments[keep_indices], rewards[:, keep_indices], preferences)
-        self._log_validation_metrics(loss, accuracy, corr_coef, sampler_metrics, prefix='held_out/')
-
+        metrics = self._validate_on_segments(segments[keep_indices], rewards[:, keep_indices], preferences)
+        self._log_validation_metrics({**metrics, 'sampler_metrics': sampler_metrics}, prefix='held_out/')
 
     def _predict_rewards(self):
         self.reward_model.eval()
@@ -293,7 +321,7 @@ class PrefPPOCallback(BasePrefCallback):
                 self.ensemble_reward_buffer[env_idx].append(
                     ensemble_rewards[:, env_idx, 0].cpu().numpy()
                 )
-            return ensemble_rewards.mean(dim=0)
+            return self.ensemble_agg_fn(ensemble_rewards)
 
 
     def _save_returns(self, infos: list):
