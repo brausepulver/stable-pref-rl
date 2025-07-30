35deeb41415d4393bbdf1357196f297d8d44e628
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-07-30 21:44:09 +0200

    feat: move aws cli and uv installation to setup script

--- .outbound/setup.sh
+++ .outbound/setup.sh
@@ -3,7 +3,14 @@ echo "TORCH_NUM_THREADS=1" >> /etc/environment
 cd /opt/workspace
 
 git clone https://token:$GITHUB_TOKEN@github.com/brausepulver/pref_rl.git .
+
+snap install --classic aws-cli
+aws sts get-caller-identity  # Verify AWS CLI installation
 aws s3 cp s3://$S3_BUCKET/validation_data.pkl /opt/workspace/data/validation_data.pkl
 
+apt update && apt install -y pipx
+pipx install uv
+pipx ensurepath
+
 export UV_CACHE_DIR=.cache/
-uv sync
+/root/.local/bin/uv sync
