d27dfe0aa1ebff3c5b9710aadecbc540b92d1e83
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-09-16 01:12:38 +0200

    fix: avoid using Snap for AWS CLI setup

--- .outbound/setup.sh
+++ .outbound/setup.sh
@@ -1,14 +1,20 @@
 echo "TORCH_NUM_THREADS=1" >> /etc/environment
 
+apt update && apt install -y pipx unzip
+
+cd /root
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
 cd /opt/workspace
 
 git clone https://token:$GITHUB_TOKEN@github.com/brausepulver/pref_rl.git .
 
-snap install --classic aws-cli
 aws sts get-caller-identity  # Verify AWS CLI installation
 aws s3 cp s3://$S3_BUCKET/validation_data.pkl /opt/workspace/data/validation_data.pkl
 
-apt update && apt install -y pipx
 pipx install uv
 pipx ensurepath
 
