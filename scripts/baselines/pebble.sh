a4a1ed021708d79885bd7bfa8e51dd346e6cb836
Johannes Kindermann <j.kindermann@campus.lmu.de>
2025-09-17 01:02:38 +0200

    feat: adjust PEBBLE experiment

--- scripts/baselines/pebble.sh
+++ scripts/baselines/pebble.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 
+N_RUNS=${1:-168}
+
 BASE_PARAMS=(
     "preset=pebble/quadruped_walk"
     "training.total_timesteps=2000000"
-    "preset.method.pref.max_feed=2000"
-    "preset.method.pref.teacher=oracle"
-    "preset.method.pref.device=cpu"
     "training.num_envs=16"
 )
 
-for i in $(seq 1 8); do
+for i in $(seq 1 $N_RUNS); do
     seed=$((1000 * i))
-    echo "Running PEBBLE with entropy sampler and seed ${seed}"
     train \
         ${BASE_PARAMS[@]} \
         training.seed=${seed} \
-        preset.method.pref.sampler=entropy \
-        "logging.group=pebble/baseline/entropy" \
-        'logging.tags=[pebble, baseline]'&
+        preset.method.pref.sampler=disagreement \
+        "logging.group=pebble/baseline/disagreement" \
+        'logging.tags=[pebble, baseline]' &
+done
+wait
+
+for i in $(seq 1 $N_RUNS); do
+    seed=$((1000 * i))
+    train \
+        ${BASE_PARAMS[@]} \
+        training.seed=${seed} \
+        preset.method.pref.sampler=disagreement \
+        "preset.method.pref.ann_buffer_size_eps=16" \
+        "+preset.method.pref.margins_stats_window_size=16" \
+        "+preset.method.pref.feed_buffer_size=200" \
+        "logging.group=pebble/optimal/disagreement" \
+        'logging.tags=[pebble, baseline]' &
 done
 wait
